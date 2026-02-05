016d934e25406dd16a7d2a9f8dd5df31628f756c
mussietarekegn <mussie.tarekegn@aastustudent.edu.et>
2026-02-06 00:21:08 +0300

    Add blog post

--- ablog/theblog/urls.py
+++ ablog/theblog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import views
+# from . import views
+from .views import HomeView,BDetailView
 
 urlpatterns = [
-    path('',views.home,name="home")
+    # path('',views.home,name="home")
+    path('',HomeView.as_view(),name="home"),
+    path('article/<int:pk>',BDetailView.as_view(),name="article-detail"),
 ]
